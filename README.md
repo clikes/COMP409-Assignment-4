e3f10b5f126e4569f31c3676c00e88ad02cefdcd
Like <chenlike695@outlook.com>
2018-04-04 09:50:39 -0400

    add assignment pdf

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # COMP409 Assignment 4
+First question is JAVA, and second one is C.
--- src/q1.java
+++ src/q1.java
@@ -1,23 +1,37 @@
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class q1 {
 	public static int[][] map = new int[30][30];
-	
+	public static AtomicIntegerArray[] AtomicMap = new AtomicIntegerArray[30];
 	
 	
 	public static void main(String[] args) {
 		
+		int n,p,r,k;
 		
+		for (int i = 0; i < 30; i++) {
+			AtomicMap[i] = new AtomicIntegerArray(30);
+		}
+		r = 100;
+		for (int i = 0; i < r; i++) {
+			int x = (int)(1+Math.random()*28);
+			int y = (int)(1+Math.random()*28);
+			if (!AtomicMap[x].compareAndSet(y, 0, 1)) {
+				i--;
+			}
+		}
 		
-		for (int[] is : map) {
+		for (AtomicIntegerArray line : AtomicMap) {
 			System.out.print("|");
-			for (int i : is) {
-				if (i == 0) {
+			for (int i = 0; i < 30; i++) {
+				int value = line.get(i);
+				if (value == 0) {
 					System.out.print(" ");//nothing
-				} else if (i == 1) {
+				} else if (value == 1) {
 					System.out.print(".");//obstacle
-				} else if (i == 2) {
+				} else if (value == 2) {
 					System.out.print("x");//goal
-				} else if (i == 3) {			
+				} else if (value == 3) {			
 					System.out.print("@");//character
 				} else {
 					System.out.print("e");//error
@@ -25,5 +39,23 @@ public class q1 {
 			}
 			System.out.println("|");
 		}
+		
+//		for (int[] is : map) {
+//			System.out.print("|");
+//			for (int i : is) {
+//				if (i == 0) {
+//					System.out.print(" ");//nothing
+//				} else if (i == 1) {
+//					System.out.print(".");//obstacle
+//				} else if (i == 2) {
+//					System.out.print("x");//goal
+//				} else if (i == 3) {			
+//					System.out.print("@");//character
+//				} else {
+//					System.out.print("e");//error
+//				}
+//			}
+//			System.out.println("|");
+//		}
 	}
 }
